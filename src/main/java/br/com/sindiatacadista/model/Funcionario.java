b6a30f2823a526352e2da4e4beef4fc26b339ea8
Hugo <hugo.nobreg@gmail.com>
2017-04-24 00:27:18 -0300

    Implementação do validador OmniFaces

--- src/main/java/br/com/sindiatacadista/model/Funcionario.java
+++ src/main/java/br/com/sindiatacadista/model/Funcionario.java
@@ -1,5 +1,7 @@
 package br.com.sindiatacadista.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,8 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@SuppressWarnings("serial")
 @Entity
-public class Funcionario {
+public class Funcionario implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -98,4 +101,36 @@ public class Funcionario {
 	public void setAtivo(String ativo) {
 		this.ativo = ativo;
 	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+	    return String.format("%s[id=%d]", getClass().getSimpleName(), getId());
+	}
+	
 }
